2d5d0f552948f598738e22e86946bbf84a56c673
Andrey <xdronmanx@bk.ru>
2016-01-28 20:02:20 +0300

    Update Zadacha11.py

--- lab11/Gogolev/Zadacha11/Zadacha11.py
+++ lab11/Gogolev/Zadacha11/Zadacha11.py
@@ -1,29 +1,29 @@
 from sys import stdin
 
-FIRST_LINE = stdin.readline()
-LIST = [int(p) for p in FIRST_LINE.split(' ')]
+first_line = stdin.readline()
+list = [int(p) for p in first_line.split(' ')]
 
 
 def SQUARE(i):
     square = 0
     j = i + 1
     p = i + 2
-    while j < len(LIST) and LIST[i] > LIST[j]:
-        square = square + (j - i)
+    while j < len(list) and list[i] > list[j]:
+        square = square + (list[i] - list[j])
         j = j + 1
         p = p + 1
-    if j != len(LIST):
+    if j != len(list):
         return square
     else:
         return 0
 
 
-def MAX_SQUARE(LIST):
+def MAX_SQUARE(list):
     s = 0
-    for x in range(0, len(LIST) - 1):
+    for x in range(0, len(list) - 1):
         if SQUARE(x) > s:
             s = SQUARE(x)
     return s
 
 
-print(MAX_SQUARE(LIST))
+print(MAX_SQUARE(list))
